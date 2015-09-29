b4a47778db5d88aae023cd47a62afc7664fa8c76
Pub4Game <Pub4Game@users.noreply.github.com>
2015-09-29 18:56:13 +0300

    Update RegionBlocker.php

--- src/minereset/RegionBlocker.php
+++ src/minereset/RegionBlocker.php
@@ -5,6 +5,7 @@ use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerMoveEvent;
 use pocketmine\level\Level;
 use pocketmine\math\Vector3;
+use pocketmine\utils\TextFormat;
 
 class RegionBlocker implements Listener{
     /** @var MineReset  */
@@ -19,7 +20,7 @@ class RegionBlocker implements Listener{
             foreach($this->activeZones[$event->getPlayer()->getLevel()->getId()] as $zone){
                 if($this->isInsideZone($event->getTo(), $zone[0], $zone[1])){
                     $event->setCancelled();
-                    $event->getPlayer()->sendMessage("You can't go in there, a mine is resetting.");
+                    $event->getPlayer()->sendMessage(TextFormat::RED . "You can't go in there, a mine is resetting.");
                     return;
                 }
             }
@@ -47,7 +48,7 @@ class RegionBlocker implements Listener{
             foreach($this->plugin->getServer()->getOnlinePlayers() as $player){
                 if($player->getLevel()->getId() === $level && $this->isInsideZone($player->getPosition(), $this->activeZones[$level][$id][0], $this->activeZones[$level][$id][1])){
                     $player->teleport($player->getSpawn());
-                    $player->sendMessage("You have been teleported because you were inside a mine when it was resetting.");
+                    $player->sendMessage(TextFormat::GREEN . "You have been teleported because you were inside a mine when it was resetting.");
                 }
             }
         }
